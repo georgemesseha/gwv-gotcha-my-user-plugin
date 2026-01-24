4de504464b914ad8211136bafe8eae8e8574d0c6
george.saeed <george.saeed@softec.ai>
2026-01-24 15:45:54 +0200

    after adding Gotcha open user plugin repo

---
+++ UserPlugin/158fdf9f02e2801c/239c2d4abb4975ce.cs
@@ -0,0 +1,8 @@
+
+partial class _158fdf9f02e2801c
+{
+    public async Task<string> GetWhatToSpeak_239c2d4abb4975ce()
+    {
+       return "Saaed 360"; 
+    }
+}
---
+++ UserPlugin/b24567603cf835ad/d14456014f8d66db.cs
@@ -0,0 +1,8 @@
+
+partial class _b24567603cf835ad
+{
+    public async Task<int> _d14456014f8d66db()
+    {
+       return 0; // return the output port index according to your logic
+    }
+}
