0234bd0480740194a0faeff4860ed3cbf13e0353
george.saeed <george.saeed@softec.ai>
2026-01-28 16:37:11 +0200

    Auto commit

---
+++ UserPlugin/1bde289216f0b707/8188da5903ddb5ba.cs
@@ -0,0 +1,8 @@
+
+partial class _1bde289216f0b707
+{
+    public async Task<string> GenerateInfoMessage_8188da5903ddb5ba()
+    {
+       return "Use your Softec credentials to log in."; // return the output port index according to your logic
+    }
+}
