1fa63dd47a7196ec63127153efc077e835141bba
george.saeed <george.saeed@softec.ai>
2026-01-29 13:21:22 +0200

    Auto commit

--- UserPlugin/74cdc26507113a04/54652d7c89ed3027.cs
+++ UserPlugin/74cdc26507113a04/54652d7c89ed3027.cs
@@ -3,6 +3,7 @@ partial class _74cdc26507113a04
 {
     public async Task<int> _54652d7c89ed3027()
     {
+        _ = _srv.VideoEditor.RepairVideosInDirectoryOnClipboardAsync();
        return 0; // return the output port index according to your logic
     }
 }
