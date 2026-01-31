7e64063f0e3d72c303a52f031dbe18abc888330b
george.saeed <george.saeed@softec.ai>
2026-01-31 20:00:12 +0200

    Auto commit

--- UserPlugin/340b866f63df92b7/3ae3b6e2523132ee.cs
+++ UserPlugin/340b866f63df92b7/3ae3b6e2523132ee.cs
@@ -3,6 +3,6 @@ partial class _340b866f63df92b7
 {
     public async Task<string> GenerateInfoMessage_3ae3b6e2523132ee()
     {
-       return "This is the template info message. You should change it."; // return the output port index according to your logic
+       return "Expect a page to open in the default browser"; // return the output port index according to your logic
     }
 }
