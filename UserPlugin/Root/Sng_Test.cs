85703c2293e1c0c4174248e224724a9622ed3537
george.saeed <george.saeed@softec.ai>
2026-03-31 18:58:49 +0200

    renaming and fixing an issue

--- UserPlugin/Root/Sng_Test.cs
+++ UserPlugin/Root/Sng_Test.cs
@@ -9,7 +9,7 @@ public class Sng_Test : SingletonService
 	public override async Task ExecuteAsync()  
 	{
 		var result = await Context.Dialog.SelectStringValueAsync("Which one?", new[] { "Mine", "Yours" }, true);
-
+	
 		if (result.isSkipped)
 		{
 			await Context.Dialog.WarningAsync("You skipped the answer");
--- UserPlugin/Softec/Projects/Sahel/Trn_Overview.cs
+++ UserPlugin/Softec/Projects/Sahel/Trn_Overview.cs
@@ -5,10 +5,11 @@ using UserPlugin.Softec.Projects.Sahel.Requirements;
 namespace UserPlugin.Softec.Projects.Sahel;
 
 [ManualTrigger("Overview", "Sahel Project")]
-public class Trn_SahelProject : TransientService
+public class Trn_Overview : TransientService
 {
 	public override async Task ExecuteAsync()
 	{
 		Context.Dialog.AddOrActivateSideWebPage("Sahel Project Overview", "https://www.notion.so/Sahel-Project-32f6ea6ef608803bb780deacf4e51ebf?source=copy_link");
+		await Context.Dialog.ShowModalMessageBoxAsync("Press Enter to exit");
 	}
 }
