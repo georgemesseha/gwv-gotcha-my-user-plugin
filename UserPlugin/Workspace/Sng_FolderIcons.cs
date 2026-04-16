e106030ffca755bc1a8cdbcd1918de2290305846
george.saeed <george.saeed@softec.ai>
2026-04-16 17:25:45 +0200

    adding folder icons

---
+++ UserPlugin/Workspace/Sng_FolderIcons.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using UserPlugin._GenericServices;
+
+namespace UserPlugin.Workspace;
+
+[ManualTrigger("Folder Icons", "Folder Icons, FI")]
+public class Sng_FolderIcons : SingletonService
+{
+	public override async Task ExecuteAsync()
+	{
+		_ = this.SpeakAsync("Opening Folder Icons");
+
+		var oneDrivePath = Resolve<Sng_SpecialFoldersRecognizer>().OneDrivePath;
+		var folderIconsPath = Path.Join(oneDrivePath, "_Explicit_", "FI");
+		var startInfo = new ProcessStartInfo
+		{
+			FileName = "cmd.exe",
+			Arguments = $"/c cd {folderIconsPath} && start explorer .",
+			UseShellExecute = false,
+			CreateNoWindow = true
+		};
+		Process.Start(startInfo);
+		
+	}
+}
